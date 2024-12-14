fb5edc683ee9fd4e62a2ed81c1bf77b163d4b558
Jane <apiwatsupaphol@gmail.com>
2024-12-15 01:40:26 +0700

    feat: add rating page and added animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          orange: "hsl(25, 97%, 53%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          lightGrey: " hsl(217, 12%, 63%)",
+          darkBlue: "hsl(213, 19%, 18%)",
+          veryDarkBlue: "hsl(216, 12%, 8%)",
+        },
+      },
+      animation: {
+        up: "up 0.5s ease-in-out",
       },
     },
   },
